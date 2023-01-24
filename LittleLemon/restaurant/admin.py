bcf9b6df60bbc10286efe290012e4886b8da6dd8
munir <munirabughannam@gmail.com>
2023-01-24 02:49:18 -0800

    fixed the models

--- LittleLemon/restaurant/admin.py
+++ LittleLemon/restaurant/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import MenuItems, Booking
 
-# Register your models here.
+admin.site.register(MenuItems)
+admin.site.register(Booking)
--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,23 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.SmallIntegerField(default=1)
+    Date = models.DateField()
+
+    def __str__(self):
+        return f'{self.Name}--->Booking Date: {self.Date}---> Guests: {self.No_of_guests}'
+
+class MenuItems(models.Model):
+    Title = models.CharField(max_length=255, db_index=True)
+    Price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    Inventory = models.SmallIntegerField(db_index=True)
+
+    class Meta:
+        verbose_name_plural = 'Menu Items'
+
+    def __str__(self):
+        return self.Title
